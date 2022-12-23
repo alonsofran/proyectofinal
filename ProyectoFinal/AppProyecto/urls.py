a712a50af1a37762eae61d719bd80db626c18e9d
alonsofran <franalonso40106@gmail.com>
2022-12-23 20:20:45 -0300

    error en el boton de cargar producto, me sale en formato texto, cuando deberia ser de tipo sumbit

--- ProyectoFinal/AppProyecto/urls.py
+++ ProyectoFinal/AppProyecto/urls.py
@@ -3,6 +3,7 @@ from AppProyecto.views import *
 
 urlpatterns = [path('producto/', producto),
 path('inicio/', inicio),
+path('productoformulario/', productoformulario, name='productoformulario'),
 
 
 ]
--- ProyectoFinal/AppProyecto/views.py
+++ ProyectoFinal/AppProyecto/views.py
@@ -10,4 +10,18 @@ def producto(request):
     return render(request, "templatepadre.html", {"producto":producto1})
 
 def inicio(request):
-    return render(request, "index.html", {})
+    return render(request, "AppProyecto/inicio.html", {})
+
+def productoformulario(request):
+    if request.method=="POST":
+        nombre=request.POST["nombre"]
+        marca=request.POST["marca"]
+        precio=request.POST["precio"]
+        producto=Producto(nombre=nombre, marca=marca, precio=precio)
+        producto.save()
+        return render(request, "AppProyecto/productoformulario.html",{})
+
+    else:
+        return render(request, "AppProyecto/productoformulario.html")
+
+

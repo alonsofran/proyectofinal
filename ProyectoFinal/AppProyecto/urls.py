0579f45d759e000e883324c1dffdeccdcc37f542
alonsofran <franalonso40106@gmail.com>
2022-12-25 20:43:53 -0300

    Primera entrega final

--- ProyectoFinal/AppProyecto/urls.py
+++ ProyectoFinal/AppProyecto/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from AppProyecto.views import *
 
-urlpatterns = [path('producto/', producto),
-path('inicio/', inicio),
-path('productoformulario/', productoformulario, name='productoformulario'),
-
-
+urlpatterns = [
+path('producto/', producto, name='producto'),
+path('', inicio, name='inicio'),
+path('ofertasformulario/', ofertasformulario, name='ofertasformulario'),
+path('productosformulario/', productosformulario, name='productosformulario'),
+path('sucursalesformulario/', sucursalesformulario, name='sucursalesformulario'),
+path('productobuscar/', productobuscar, name='productobuscar'),
+path('buscarp/', buscarp, name='buscarp'),
 ]
--- ProyectoFinal/ProyectoFinal/urls.py
+++ ProyectoFinal/ProyectoFinal/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from AppProyecto.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio, name='inicio'),
     path('AppProyecto/', include('AppProyecto.urls')),
 ]

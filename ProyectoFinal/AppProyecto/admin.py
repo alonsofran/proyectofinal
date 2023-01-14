0537b1b4d1e18c3062bf42c75b60a94c31c78b3a
alonsofran <119707182+alonsofran@users.noreply.github.com>
2023-01-14 13:22:11 -0300

    Delete ProyectoFinal directory

--- ProyectoFinal/AppProyecto/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Producto, Ofertas, Sucursales
-
-# Register your models here.
-
-admin.site.register(Producto)
-admin.site.register(Ofertas)
-admin.site.register(Sucursales)
